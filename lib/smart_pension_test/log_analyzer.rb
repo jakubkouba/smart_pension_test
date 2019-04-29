4be72e8208262d957a05b03c38f6c649c7e68b8a
Jakub Adler <adler.jakub@gmail.com>
2019-04-29 15:46:28 +0100

    LogAnalyser#most_page_views returns one item with path "a" and visit count 1

--- lib/smart_pension_test/log_analyzer.rb
+++ lib/smart_pension_test/log_analyzer.rb
@@ -1,12 +1,16 @@
 module SmartPensionTest
   class LogAnalyzer
 
+    attr_reader :parsed_log_data
+
     def initialize(parsed_log_data)
       @parsed_log_data = parsed_log_data
     end
 
     def most_page_views
-      @parsed_log_data
+      return [] if parsed_log_data.empty?
+
+      [{ path: parsed_log_data.first.path, visit_count: 1 }]
     end
   end
 end
--- spec/smart_pension_test/log_analyzer_spec.rb
+++ spec/smart_pension_test/log_analyzer_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe SmartPensionTest::LogAnalyzer do
       log_analyzer = described_class.new([])
       expect(log_analyzer.most_page_views).to be_kind_of Enumerable
     end
+
+    context 'log contains one item' do
+
+      it 'contains one item with path a and visit count 1' do
+        parsed_log_item = [double('LogItem', path: 'a', ip_address: 1)]
+        log_analyzer = described_class.new(parsed_log_item)
+        expect(log_analyzer.most_page_views).to match([path: 'a', visit_count: 1])
+      end
+    end
   end
 
 end
