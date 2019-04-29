132f10bc94b9b3a8d2dccdabd39479ffa406475b
Jakub Adler <adler.jakub@gmail.com>
2019-04-29 13:39:44 +0100

    LogParser#data returns one item if log file contains one log item

--- lib/smart_pension_test/log_parser.rb
+++ lib/smart_pension_test/log_parser.rb
@@ -1,12 +1,14 @@
 module SmartPensionTest
   class LogParser
 
+    attr_reader :file
+
     def initialize(file)
       @file = file
     end
 
     def data
-      []
+      File.readlines(file, chomp: true)
     end
   end
 end
--- spec/smart_pension_test/log_parser_spec.rb
+++ spec/smart_pension_test/log_parser_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe SmartPensionTest::LogParser do
-  let(:log_file) { File.expand_path('support/webserver.log') }
+  let(:log_file) { File.expand_path('spec/support/webserver.log') }
+  let(:log_parser) { described_class.new(log_file) }
 
   it 'takes a log file as an initialize argument' do
     described_class.new(log_file)
@@ -8,8 +9,15 @@ RSpec.describe SmartPensionTest::LogParser do
   describe '#data' do
 
     it 'is an array' do
-      log_parser = described_class.new(log_file)
       expect(log_parser.data).to be_an Array
+    end
+
+    context 'when log file contains one log item' do
+      let(:log_file) { File.expand_path('spec/support/file_with_one_log_item.log') }
+
+      it 'has one item' do
+        expect(log_parser.data.count).to eq 1
+      end
     end
   end
 end
