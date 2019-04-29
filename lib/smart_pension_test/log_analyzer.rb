fb962f600998f6d9688298004485aba55bb2599d
Jakub Adler <adler.jakub@gmail.com>
2019-04-29 14:43:37 +0100

    LogAnalyser#most_page_views return enumerable

--- lib/smart_pension_test/log_analyzer.rb
+++ lib/smart_pension_test/log_analyzer.rb
@@ -4,5 +4,9 @@ module SmartPensionTest
     def initialize(parsed_log_data)
       @parsed_log_data = parsed_log_data
     end
+
+    def most_page_views
+      @parsed_log_data
+    end
   end
 end
--- spec/smart_pension_test/log_analyzer_spec.rb
+++ spec/smart_pension_test/log_analyzer_spec.rb
@@ -3,4 +3,13 @@ RSpec.describe SmartPensionTest::LogAnalyzer do
   it 'takes parsed log file as an initialize argument' do
     described_class.new([])
   end
+
+  describe '#most_page_views' do
+
+    it 'is an collection' do
+      log_analyzer = described_class.new([])
+      expect(log_analyzer.most_page_views).to be_kind_of Enumerable
+    end
+  end
+
 end
