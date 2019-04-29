c7b218d75aab1b728bc64432fd6b93e4031540f0
Jakub Adler <adler.jakub@gmail.com>
2019-04-29 13:12:21 +0100

    Remove default failing spec

--- spec/smart_pension_test_spec.rb
+++ spec/smart_pension_test_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe SmartPensionTest do
   it "has a version number" do
     expect(SmartPensionTest::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
