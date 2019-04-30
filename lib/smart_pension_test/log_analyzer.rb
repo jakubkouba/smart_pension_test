accff28d2a8c4c45087d8899178d49c12f906ea0
Jakub Adler <adler.jakub@gmail.com>
2019-04-30 01:09:24 +0100

    Remove if else condition from LogAnalyzer#most_page_views

--- lib/smart_pension_test/log_analyzer.rb
+++ lib/smart_pension_test/log_analyzer.rb
@@ -13,17 +13,13 @@ module SmartPensionTest
     def most_page_views
       return [] if parsed_log_data.empty?
 
-      list = parsed_log_data.each_with_object([]) do |log_item, list|
-        path_in_list = list.detect { |item| item[:path] == log_item.path }
-
-        if path_in_list
-          list[list.index(path_in_list)][:visit_count] += 1
-        else
-          list << { path: log_item.path, visit_count: 1 }
-        end
+      visit_count = parsed_log_data.each_with_object({}) do |log_item, visit_count|
+        visit_count[log_item.path] ||= 0
+        visit_count[log_item.path] += 1 if visit_count[log_item.path]
       end
 
-      list.sort_by { |item| item[:visit_count] }.reverse
+      visit_count = visit_count.map { |path, visit_count| { path: path, visit_count: visit_count } }
+      visit_count.sort_by { |item| item[:visit_count] }.reverse
     end
 
     def most_unique_page_views
